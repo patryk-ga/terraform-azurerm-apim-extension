6164cc0c4d63c2e520d4355adc2486abd0cf92ce
Rutger Buiteman <rutger.buiteman@xebia.com>
2024-12-06 10:43:23 +0100

    Added basic code for APIM workspace deployment

--- src/main.tf
+++ src/main.tf
@@ -1,11 +1,44 @@
-resource "azurerm_resource_group" "this" {
-  count    = var.create_resource_group ? 1 : 0
-  location = var.module_location
-  name     = var.resource_group_name
+resource "azapi_resource" "apim_workspace" {
+  type      = "Microsoft.ApiManagement/service/workspaces@2024-06-01-preview"
+  name      = var.apim_workspace_name
+  parent_id = var.apim_resource_id
 
-  tags = merge(
-    var.module_tags,
-    var.resource_group_tags
-  )
+  body = {
+    properties = {
+      description = var.apim_workspace_description
+      displayName = var.apim_workspace_name
+    }
+  }
 }
 
+resource "azurerm_role_assignment" "sp_apim_workspace_contributor" {
+  depends_on = [azapi_resource.apim_workspace]
+
+  principal_id         = var.service_principal_object_id
+  role_definition_name = "API Management Workspace Contributor"
+  scope                = azapi_resource.apim_workspace.id
+}
+
+resource "azurerm_role_assignment" "owner_group_apim_workspace_contributorr" {
+  depends_on = [azapi_resource.apim_workspace]
+
+  principal_id         = var.owner_group_id
+  role_definition_name = "API Management Workspace Contributor"
+  scope                = azapi_resource.apim_workspace.id
+}
+
+resource "azurerm_role_assignment" "contributor_group_apim_workspace_contributor" {
+  depends_on = [azapi_resource.apim_workspace]
+
+  principal_id         = var.contributor_group_id
+  role_definition_name = "API Management Workspace Contributor"
+  scope                = azapi_resource.apim_workspace.id
+}
+
+resource "azurerm_role_assignment" "reader_group_apim_workspace_reader" {
+  depends_on = [azapi_resource.apim_workspace]
+
+  principal_id         = var.reader_group_id
+  role_definition_name = "API Management Workspace Reader"
+  scope                = azapi_resource.apim_workspace.id
+}
--- src/variables_global.tf
+++ src/variables_global.tf
@@ -1,10 +1,19 @@
-variable "module_location" {
-  description = "The Azure region where the resource group (RG) should exist. Changing this forces a new RG to be created."
+variable "service_principal_object_id" {
+  description = "The ID of the service principal of the spoke."
   type        = string
 }
 
-variable "module_tags" {
-  description = "A mapping of tags which should be assigned to all resources of the module."
-  type        = map(string)
-  default     = null
+variable "owner_group_id" {
+  description = "The ID of the PIM Owner group of the spoke."
+  type        = string
+}
+
+variable "contributor_group_id" {
+  description = "The ID of the PIM Contributor group of the spoke."
+  type        = string
+}
+
+variable "reader_group_id" {
+  description = "The ID of the PIM Reader group of the spoke."
+  type        = string
 }
